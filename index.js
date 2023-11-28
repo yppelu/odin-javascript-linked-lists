c51d71469830f3848b75750ff20c6b038ba5a3a2
yppelu <yppelu@gmail.com>
2023-11-28 17:39:09 +0700

    Add method append

--- index.js
+++ index.js
@@ -11,4 +11,14 @@ class LinkedList {
     this._tail = null;
     this._size = 0;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this._size) this._head = newNode;
+    else this._tail.nextNode = newNode;
+    this._tail = newNode;
+
+    this._size++;
+  }
 }
